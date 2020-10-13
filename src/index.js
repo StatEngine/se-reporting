4e2898d091b641e0dceb2b608967ffad120ff7b7
paul altefogt <paul@prominentedge.com>
2020-10-13 09:37:49 -0400

    handle daylight savings time

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import later from 'later';
 import _ from 'lodash';
 
 import config from './config';
+import timerStore from './timer-store';
 import { schedule } from './lib/scheduler/scheduler';
 
 // schedule everything in UTC
@@ -16,16 +17,46 @@ function getEmailReportConfiguration() {
   return request(options);
 }
 
-getEmailReportConfiguration()
-  .then((periodics) => {
-    periodics.forEach((periodic) => {
-      if (!_.isNil(periodic.enabled) && periodic.enabled === false) return;
+// if daylight savings time, then
+// subtract an hour from the run time
+function setTimeDst(sched) {
+  let time = sched.schedules[0].t[0];
+  // later uses seconds, not miliseconds
+  time = time - 3600;
+  return [time];
+}
 
-      const periodicConfig = periodic.config_json;
-      if (_.get(periodicConfig, 'schedulerOptions.later.text')) {
-        const sched = later.parse.text(periodicConfig.schedulerOptions.later.text);
-
-        schedule(periodic._id, sched, 'EmailReport', periodic);
-      }
+function scheduleAll(isDst) {
+  getEmailReportConfiguration()
+    .then((periodics) => {
+      periodics.forEach((periodic) => {
+        if (!_.isNil(periodic.enabled) && periodic.enabled === false) return;
+        const periodicConfig = periodic.config_json;
+        if (_.get(periodicConfig, 'schedulerOptions.later.text')) {
+          let schedText = periodicConfig.schedulerOptions.later.text;
+          const sched = later.parse.text(schedText);
+          if (isDst) {
+            // if daylight savings time, then we need to update
+            // the schedule time
+            sched.schedules[0].t = setTimeDst(sched);
+          }
+          schedule(periodic._id, sched, 'EmailReport', periodic);
+        }
+      });
     });
-  });
+}
+
+// when this container starts up, we want to run scheduleAll on the days that daylight
+// savings time starts and ends
+// daylight savings time starts on the second Sunday of March
+const startDstSchedule = later.parse.recur().on(3).month().on(2).weekOfMonth().on(1).hour();
+// daylight savings time ends on the first Sunday of November
+const endDstSchedule = later.parse.recur().on(11).month().on(1).weekOfMonth().on(1).dayOfWeek().on(1).hour();
+
+const startDstInterval = later.setInterval(scheduleAll(true), startDstSchedule);
+const endDstInterval = later.setInterval(scheduleAll(false), endDstSchedule);
+
+timerStore.addInterval('startDst', startDstInterval);
+timerStore.addInterval('endDst', endDstInterval);
+
+
