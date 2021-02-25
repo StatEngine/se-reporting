e6b7681dd53fdc65ea21011247db485ba83e9e48
paul altefogt <paul@prominentedge.com>
2021-02-25 16:51:36 -0500

    feat(email scheduling): add functionality for scheduling custom emails

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import request from 'request-promise';
 import later from 'later';
 import _ from 'lodash';
@@ -9,19 +10,37 @@ import { schedule } from './lib/scheduler/scheduler';
 // schedule everything in UTC
 later.date.UTC();
 
-function getEmailReportConfiguration() {
+/**
+ * does GET requests to the webapp api for email configs
+ * @param {string} path this is the path that gets appended to the baseUrl
+ */
+async function getEnabledEmailConfigs(path) {
   const options = _.cloneDeep(config.statengine);
-  options.uri += '/extension-configurations?name=Email Report';
+  options.uri += path;
   options.json = true;
+  const emailConfigs = await request(options);
+  return emailConfigs.filter(emailConfig => emailConfig.enabled);
+}
 
-  return request(options);
+/**
+ * retrieve the notification email configs from the webapp api
+ */
+function getNotificationEmailConfigs() {
+  return getEnabledEmailConfigs('/extension-configurations?name=Email Report');
+}
+
+/**
+ * retrieve the custom email configs from the webapp api.
+ */
+function getCustomEmailConfigs() {
+  return getEnabledEmailConfigs('/custom-emails');
 }
 
 // UTC does not account for DST hence the offset between US Eastern and UTC time changes
 // from -5hrs to -4 hrs during DST (approx March-Nov)
 // Hence, if it is not currently DST, then we need to add an hour offset to the schedule
-// since the clock has "fallen back". It is also important to remember that the times/schedules specified
-// in the DB configs assume a DST offset.
+// since the clock has "fallen back". It is also important to remember that the times/schedules
+// specified in the DB configs assume a DST offset.
 // e.g.
 // Richmond wants emails at approx 8:00 am EST
 // Their config specifies approx 12:00 pm UTC
@@ -46,46 +65,78 @@ function shouldSubtractAnHour(deptId) {
     '97477', // Tuscon, AZ
   ];
 
-  // if a DST department && it is not currently DST, then we need to offset the time (see comments for subtractAnHour method)
+  /**
+   * if a DST department && it is not currently DST, then we need to offset the time
+   * (see comments for subtractAnHour method)
+   */
   return (nonDSTDepartments.findIndex(d => d === deptId) === -1 && moment().isDST() === false);
 }
 
-function sendDailyEmail(sendTime) {
-  console.log(`sendDailyEmail : ${sendTime}`);
-
-  getEmailReportConfiguration()
-    .then((periodics) => {
-      periodics.forEach((periodic) => {
-        if (!_.isNil(periodic.enabled) && periodic.enabled === false) return;
-        const periodicConfig = periodic.config_json;
-        if (_.get(periodicConfig, 'name') === 'Daily') {
-          const sched = later.parse.text(sendTime);
-          schedule(periodic._id, sched, 'EmailReport', periodic);
-        }
-      });
-    });
+/**
+ * creates a schedule using laterjs. accounts for daylight savings time
+ * @param {string} schedText this is a string that laterjs will parse into a schedule
+ * @param {string} deptId this is the fd_id of the fire department that owns the email
+ */
+function getScheduleWithDST(schedText, deptId) {
+  const sched = later.parse.text(schedText);
+  if (shouldSubtractAnHour(deptId)) {
+    sched.schedules[0].t = subtractAnHour(sched);
+  }
+  return sched;
 }
 
-function scheduleAll() {
-  getEmailReportConfiguration()
-    .then((periodics) => {
-      periodics.forEach((periodic) => {
-        if (!_.isNil(periodic.enabled) && periodic.enabled === false) return;
-        const periodicConfig = periodic.config_json;
-        if (_.get(periodicConfig, 'schedulerOptions.later.text')) {
-          const schedText = periodicConfig.schedulerOptions.later.text;
-          const sched = later.parse.text(schedText);
-          const deptId = periodic.fire_department__id;
-          if (shouldSubtractAnHour(deptId)) {
-            sched.schedules[0].t = subtractAnHour(sched);
-          }
-          schedule(periodic._id, sched, 'EmailReport', periodic);
-        }
-      });
-    });
+/**
+ * handles scheduling of the standard notification emails
+ * @param {*} emailConfig the email options object. these are stored in the
+ * db in table ExtensionConfigurations.
+ * see the se-sql-seed repo for each department's options
+ */
+function scheduleNotificationEmail(emailConfig) {
+  const periodicConfig = emailConfig.config_json;
+  if (_.get(periodicConfig, 'schedulerOptions.later.text')) {
+    const schedText = periodicConfig.schedulerOptions.later.text;
+    const deptId = emailConfig.fire_department__id;
+    const sched = getScheduleWithDST(schedText, deptId);
+    schedule(emailConfig._id, sched, 'EmailReport', emailConfig);
+  }
 }
 
-// go ahead and run the schedule immediately, and it will handle whatever the current
+/**
+ * handles scheduling of the custom emails that are created by the users of the webapp
+ * @param {*} emailConfig the email options object. these are stored in the db in table CustomEmails
+ */
+function scheduleCustomEmail(emailConfig) {
+  const { __id: configId, fd_id: deptId, schedule: schedText } = emailConfig;
+  const sched = getScheduleWithDST(schedText, deptId);
+  schedule(configId, sched, 'CustomEmail', emailConfig);
+}
+
+/**
+ * this handles scheduling all emails
+ */
+async function scheduleAll() {
+  const notificationEmailConfigs = await getNotificationEmailConfigs()
+    .catch((err) => {
+      console.log('ERROR: getEmailReportConfiguration');
+      console.dir(err);
+    });
+  notificationEmailConfigs.forEach((emailConfig) => {
+    scheduleNotificationEmail(emailConfig);
+  });
+
+  // this handles scheduling the custom emails
+  const customEmailConfigs = await getCustomEmailConfigs()
+    .catch((err) => {
+      console.log('ERROR: getEmailReportConfiguration');
+      console.dir(err);
+    });
+
+  customEmailConfigs.forEach((emailConfig) => {
+    scheduleCustomEmail(emailConfig);
+  });
+}
+
+// go ahead and run scheduleAll immediately, and it will handle whatever the current
 // state of DST is
 scheduleAll();
 
@@ -107,9 +158,3 @@ const endDstSchedule = later.parse.recur().on(11).month().on(1)
 // we want to reschedule on start and end of DST
 later.setInterval(scheduleAll, startDstSchedule);
 later.setInterval(scheduleAll, endDstSchedule);
-
-// these next two lines are ONLY for resending the daily emails if they don't
-// go out for some reason
-// remember that times are all in UTC
-// const emailSendTime = 'at 4:30 pm';
-// sendDailyEmail(emailSendTime);
